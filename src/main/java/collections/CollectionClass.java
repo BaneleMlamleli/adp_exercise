c446470e09a7e647f29c2bb497355172bfa13abb
BaneleMlamleli <mlamlelibanele@yahoo.com>
2019-03-02 16:24:44 +0200

    Added stand alone class for all collections and a test file for the class

---
+++ src/main/java/collections/CollectionClass.java
@@ -0,0 +1,24 @@
+package collections;
+
+public class CollectionClass {
+
+    // This method implements Collection
+    public void implementCollection(){
+
+    }
+
+    // This method implements List
+    public void implementList(){
+
+    }
+
+    // This method implements Set
+    public void implementSet(){
+
+    }
+
+    // This method implements Map
+    public void implementMap(){
+
+    }
+}
